581c85b189113bfa4221e0f48454f5e47ad17ab2
rvillarreal-r7 <ryan_villarreal@rapid7.com>
2023-06-02 19:56:50 -0400

    backup

--- pumpkin.py
+++ pumpkin.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-# This file is intentionally left blank to prevent complexities 
 
 def main():
-	# setup logger early 
 	from utils import logger
 	log = logger.LogAdapter(__name__)
 
-	# parse args and dowork
 	from cli import cmdline,args
 
 if __name__ == "__main__":
