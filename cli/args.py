3c9a219f503f219b15db51f2d4582679cc003e7e
rvillarreal-r7 <ryan_villarreal@rapid7.com>
2023-06-13 14:56:24 -0400

    reworked cmdline to be more intuitive and added verbose flags

--- cli/args.py
+++ cli/args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-### external imports ###
-import argparse
-
 ### local imports ###
-from utils import logger
-from devices import device
 from cli import cmdline
+from utils import logger
 log = logger.LogAdapter(__name__)
+
+### external imports ###
+import argparse,logging
 
 # Create the top-level parser
 parser = argparse.ArgumentParser(description="Pumpkin", add_help=True)
@@ -15,17 +14,20 @@ subparsers = parser.add_subparsers(dest='command', help='Available commands')
 
 # Create the parser for the "cmd" command
 cmd_parser = subparsers.add_parser('cmd', help='Execute the cmdline tooling')
+cmd_parser.add_argument('--verbose', '-v', action='store_true') # I really hate this...
 cmd_subparser = cmd_parser.add_mutually_exclusive_group(required=True)
 cmd_subparser.add_argument('--search','-s', dest='searchTerm', help='Search the AppStore for a bundleId')
 cmd_subparser.add_argument('--lookup','-l',dest='lookupTerm', help='Lookup App by bundleId [com.example.App]',)
 
 # Create the parser for the "api" command
 api_parser = subparsers.add_parser('api', help='Execute the API server')
+api_parser.add_argument('--verbose', '-v', action='store_true') # I really hate this...
 api_subparser = api_parser.add_mutually_exclusive_group()
 api_subparser.add_argument('--info', action='store_true', help='Get list of connected devices',)
 
 # Create the parser for the "device" command
 device_parser = subparsers.add_parser('device', help='Perform device operations')
+device_parser.add_argument('--verbose', '-v', action='store_true') # I really hate this...
 device_subparser = device_parser.add_mutually_exclusive_group(required=True)
 device_subparser.add_argument('--info',action='store_true', help='Get list of connected devices')
 device_subparser.add_argument('--list',action='store_true', help='Get list of connected devices')
@@ -33,11 +35,11 @@ device_subparser.add_argument('--list',action='store_true', help='Get list of co
 # Parse the command-line arguments
 args = parser.parse_args()
 
+if args.verbose:
+    logger.update_logger(logging.DEBUG) # if we have verbose flag increease logging level
+
 if args.command:
     # Three core CLI options [cli,api,dev]
-    #  cli: run the tooling in CLI mode with local USB devices
-    #  api: run the tooling in headless mode
-    #  dev: diagnostics of connected USB devices
     if args.command == 'cmd':
         log.debug("Starting cmdline mode")
         if args.searchTerm:
@@ -50,7 +52,6 @@ if args.command:
 
     elif args.command == 'device':
         # log.debug("Loading Device Diagnostics")
-        log.halt("device cmdline")
         cmdline.devices(args)
     else:
         log.error("Ruh Roh Raggy")
--- pumpkin.py
+++ pumpkin.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
 def main():
-	from utils import logger
-	log = logger.LogAdapter(__name__)
-
-	from cli import cmdline,args
+	from cli import cmdline,args 
 
 if __name__ == "__main__":
 	main()
